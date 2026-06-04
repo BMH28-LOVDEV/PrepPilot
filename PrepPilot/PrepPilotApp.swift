a93771eb4f7f9ee1e3fa422cf037f78517527de4
Benjamin M. Hale <benjaminmhale@icloud.com>
2026-06-03 22:44:14 -0700

    Initial Commit

---
+++ PrepPilot/PrepPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrepPilotApp.swift
+//  PrepPilot
+//
+//  Created by Benjamin M. Hale on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PrepPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
